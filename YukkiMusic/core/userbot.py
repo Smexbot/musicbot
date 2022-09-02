e1dcdaacc0e88663c92e38731c81c7d3262adebb
Smexbot <112428577+Smexbot@users.noreply.github.com>
2022-09-02 19:45:28 +0530

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -57,9 +57,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("TheDeadlyBots")
-                await self.one.join_chat("TheBotUpdates")
-                await self.one.join_chat("THE_DEADLY_NETWORK")
+                await self.one.join_chat("BHOOT_SUPPORT")
+                await self.one.join_chat("BHOT_UPDATE")
             except:
                 pass
             assistants.append(1)
@@ -88,9 +87,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("TheDeadlyBots")
-                await self.two.join_chat("TheBotUpdates")
-                await self.two.join_chat("THE_DEADLY_NETWORK")
+                await self.one.join_chat("BHOOT_SUPPORT")
+                await self.one.join_chat("BHOT_UPDATE")
             except:
                 pass
             assistants.append(2)
@@ -119,9 +117,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("TheDeadlyBots")
-                await self.three.join_chat("TheBotUpdates")
-                await self.three.join_chat("THE_DEADLY_NETWORK")
+                await self.one.join_chat("BHOOT_SUPPORT")
+                await self.one.join_chat("BHOT_UPDATE")
             except:
                 pass
             assistants.append(3)
@@ -150,9 +147,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("TheDeadlyBots")
-                await self.four.join_chat("TheBotUpdates")
-                await self.four.join_chat("THE_DEADLY_NETWORK")
+                await self.one.join_chat("BHOOT_SUPPORT")
+                await self.one.join_chat("BHOT_UPDATE")
             except:
                 pass
             assistants.append(4)
@@ -181,9 +177,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("TheDeadlyBots")
-                await self.five.join_chat("TheBotUpdates")
-                await self.five.join_chat("THE_DEADLY_NETWORK")
+                await self.one.join_chat("BHOOT_SUPPORT")
+                await self.one.join_chat("BHOT_UPDATE")
             except:
                 pass
             assistants.append(5)
